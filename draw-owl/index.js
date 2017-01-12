49e4547a7b4c06b7b8115249616db79e8d03b358
Josh <cbcldowns@gmail.com>
2017-01-11 22:10:11 -0800

    working desktop

---
+++ draw-owl/index.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app);
+
+server.listen(8000);
+console.log('Server listening on port 8000');
+console.log('Point your browser to http://localhost:8000');
+
+app.use(express.static('public'));
