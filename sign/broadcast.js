9749922ccc8c7c269ad9858977e2f7652383bc43
Josh <cbcldowns@gmail.com>
2017-01-11 23:39:57 -0800

    status

---
+++ sign/broadcast.js
@@ -0,0 +1,17 @@
+var Kinect2 = require('kinect2'),
+    express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+var kinect = new Kinect2();
+
+if(kinect.open()) {
+    server.listen(5555);
+
+    kinect.on('bodyFrame', function(bodyFrame){
+        io.sockets.emit('bodyFrame', bodyFrame);
+    });
+
+    kinect.openBodyReader();
+}
